9f021850d395c95f53b00cc1ed3df3d8a5f26825
PatrykKuniczak <p.kuniczak@gmail.com>
2024-09-11 18:24:18 +0200

    fix: change last forgotten __FIREFOX__ env

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -3,7 +3,7 @@ import deepmerge from 'deepmerge';
 
 const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
-const isFirefox = process.env.__FIREFOX__ === 'true';
+const isFirefox = process.env.CLI_CEB_FIREFOX === 'true';
 
 /**
  * If you want to disable the sidePanel, you can delete withSidePanel function and remove the sidePanel HoC on the manifest declaration.
