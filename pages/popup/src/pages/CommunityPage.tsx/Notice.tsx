28699cfa3eaed2caff5ab80948cc8714f8e89256
thy-lenk-otsv <thy.lenk@one-line.com>
2025-01-10 09:07:38 +0700

    feat: add create community page

---
+++ pages/popup/src/pages/CommunityPage.tsx/Notice.tsx
@@ -0,0 +1,87 @@
+import { Button, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { BootstrapInput, StyledLabel } from '@src/components/Input';
+import React from 'react';
+
+function Notice() {
+  return (
+    <div className="px-3">
+      <h1 className="px-0 py-1 text-lg mx-0 my-0">Notice of a focal point</h1>
+      <h2 className="px-0 py-1 text-xs mx-0 my-0 font-normal">Following these steps to inform us about the problem</h2>
+      <form className="flex flex-col gap-6 mt-2">
+        <FormControl variant="standard" required fullWidth>
+          <StyledLabel shrink htmlFor="bootstrap-input" className="text-neutral-700">
+            Location
+          </StyledLabel>
+          <BootstrapInput id="bootstrap-input" fullWidth />
+        </FormControl>
+
+        <FormControl variant="standard" required fullWidth>
+          <StyledLabel shrink htmlFor="bootstrap-input" className="text-neutral-700">
+            Focal point name
+          </StyledLabel>
+          <BootstrapInput id="bootstrap-input" fullWidth />
+        </FormControl>
+
+        <FormControl variant="standard" required fullWidth>
+          <StyledLabel shrink htmlFor="bootstrap-input" className="text-neutral-700">
+            Representative
+          </StyledLabel>
+          <BootstrapInput id="bootstrap-input" fullWidth />
+        </FormControl>
+
+        <FormControl variant="standard" fullWidth>
+          <StyledLabel shrink htmlFor="bootstrap-input" className="text-neutral-700">
+            Description
+          </StyledLabel>
+          <BootstrapInput id="bootstrap-input" fullWidth />
+        </FormControl>
+
+        <FormControl variant="standard" fullWidth required>
+          <StyledLabel shrink htmlFor="bootstrap-input" className="text-neutral-700">
+            Category
+          </StyledLabel>
+          {/* <BootstrapInput id="bootstrap-input" fullWidth /> */}
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            sx={{
+              mt: 2,
+              '& .MuiSvgIcon-root': {
+                fontSize: 14,
+              },
+            }}>
+            <FormControlLabel
+              value="people"
+              control={<Radio />}
+              label="People"
+              slotProps={{
+                typography: {
+                  fontSize: 14,
+                  fontWeight: '400',
+                },
+              }}
+            />
+            <FormControlLabel value="planet" control={<Radio />} label="Planet" />
+            <FormControlLabel
+              value="workspace"
+              control={<Radio />}
+              label="Workspace"
+              slotProps={{
+                typography: {
+                  fontSize: 14,
+                  fontWeight: '400',
+                },
+              }}
+            />
+          </RadioGroup>
+        </FormControl>
+
+        <Button color="primary" variant="contained" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Notice;
