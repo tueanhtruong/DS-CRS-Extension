76e25bcb707ef86e5405613b802db6f5e1751f17
thy-lenk-otsv <thy.lenk@one-line.com>
2025-01-10 09:12:33 +0700

    feat: add create community page

--- pages/popup/src/pages/CommunityPage.tsx/index.tsx
+++ pages/popup/src/pages/CommunityPage.tsx/index.tsx
@@ -1,19 +1,25 @@
-import { Button, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
-import { BootstrapInput, StyledLabel } from '@src/components/Input';
-import Create from '@src/pages/CommunityPage.tsx/Create';
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 function CommunityPage() {
+  const navigate = useNavigate();
   return (
-    // <div className='px-3'>
-    //   <h1 className="px-0 py-1 text-lg mx-0 my-0">Create an event</h1>
-    //   <div className="flex flex-col gap-2">
-    //     <h2 className="px-0 py-1 text-xs mx-0 my-0">What is your purpose</h2>
-    //     <button className="text-neutral-700 bg-white px-4 py-2 text-sm w-full rounded">Notice of a focal point</button>
-    //     <button className="text-neutral-700 bg-white px-4 py-2 text-sm w-full rounded">Create an event</button>
-    //   </div>
-    // </div>
-    <Create />
+    <div className="px-3">
+      <h1 className="px-0 py-1 text-lg mx-0 my-0">Create an event</h1>
+      <div className="flex flex-col gap-2">
+        <h2 className="px-0 py-1 text-xs mx-0 my-0">What is your purpose</h2>
+        <button
+          className="text-neutral-700 bg-white px-4 py-2 text-sm w-full rounded"
+          onClick={() => navigate('notice')}>
+          Notice of a focal point
+        </button>
+        <button
+          className="text-neutral-700 bg-white px-4 py-2 text-sm w-full rounded"
+          onClick={() => navigate('create')}>
+          Create an event
+        </button>
+      </div>
+    </div>
   );
 }
 
